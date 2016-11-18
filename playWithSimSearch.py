45c072ebee80eeedafd1db3e0af85853c544c4f9
chrisjmccormick <chrisjmccormick@gmail.com>
2016-11-18 14:09:13 -0800

    Added second example - k-means clustering of corpus

--- playWithSimSearch.py
+++ playWithSimSearch.py
@@ -2,40 +2,89 @@
 """
 Created on Tue Oct 18 16:29:31 2016
 
-@author: Chris
+@author: Chris McCormick
 """
 
 from simsearch import SimSearch
+from sklearn.cluster import KMeans
+
+def runExample1(ssearch):
+    """
+    ======== Example 1 ========
+    Find documents similar to 'document' number 73, which is mhc1.txt lines 
+    1617 - 1647. This is commentary on the seventh day of creation, when God 
+    rested. The top match is commentary on the fourth commandment--to obey the 
+    sabbath (Exodus Chapter 20).
+    """
+    
+    print('Searching for docs similar to document number 73...')
+    print('')
+    
+    # Display the source document.
+    print('Doc 73:')
+    ssearch.cb.printDocSourcePretty(doc_id=73, max_lines=5)
+    
+    print('')
+    
+    # Perform the search
+    results = ssearch.findSimilarToDoc(doc_id=73, topn=1)
+    
+    # Print the top results
+    ssearch.printResultsBySourceText(results, max_lines=8)
+    
+    # Retrieve the tf-idf vectors for the input document and it's closest match.
+    vec1_tfidf = ssearch.cb.getDocTfidfVector(73)
+    vec2_tfidf = ssearch.cb.getDocTfidfVector(results[0][0])
+    
+    # Interpret the top match.
+    ssearch.interpretMatch(vec1_tfidf, vec2_tfidf)
+
+def runExample2(ssearch):
+    """
+    ======== Example 2 ========
+    Clusters the entire corpus using k-means, then displays the top words
+    for each cluster.
+    
+    Some fun results that I've seen:
+      Theme of money:
+        "poor rich charity man riches good poverty wealth money christ"
+      Theme of sin:
+        "sin sins god repentance sinners iniquity evil ruin judgments 
+         punishment"
+      Theme of Christ's resurrection:
+        "death life dead die body christ resurrection live grave soul"
+    """
+    
+    # Number of clusters to find...
+    k = 20    
+    
+    print('Clustering all documents in corpus...')
+    # Initialize a k-means clustering object.
+    km = KMeans(n_clusters=k, init='k-means++', max_iter=100)
+    
+    # Cluster the LSI vectors.     
+    km.fit(ssearch.index.index)
+
+    # For each of the clusters...    
+    for cluster_id in range(0, k):
+        
+        # Get the list of all doc IDs belonging to this cluster.
+        cluster_doc_ids = []
+        for doc_id in range(0, len(km.labels_)):            
+            if km.labels_[doc_id] == cluster_id:
+                cluster_doc_ids.append(doc_id)
+
+        # Get the top words in this cluster
+        top_words = ssearch.getTopWordsInCluster(cluster_doc_ids)
+
+        print('  Cluster %d: %s' % (cluster_id, " ".join(top_words)))
+
 
 # Load the pre-built corpus.
-print 'Loading the saved SimSearch and corpus...'
+print('Loading the saved SimSearch and corpus...')
 ssearch = SimSearch.load(save_dir='./mhc_corpus/')
 
-
-# Find documents similar to 'document' number 73, which is mhc1.txt lines 1617
-# - 1647. This is commentary on the seventh day of creation, when God rested.
-# The top match is commentary on the fourth commandment--to obey the sabbath.
-# (Exodus Chapter 20).
-print ''
-print 'Searching for docs similar to document number 73...'
-print ''
-
-# Display the source document.
-print 'Doc 73:'
-lines = ssearch.cb.printDocSourcePretty(doc_id=73, max_lines=5)
-
-print ''
-
-# Perform the search
-results = ssearch.findSimilarToDoc(doc_id=73, topn=1)
-
-# Print the top results
-ssearch.printResultsBySourceText(results, max_lines=8)
-
-# Retrieve the tf-idf vectors for the input document and it's closest match.
-vec1_tfidf = ssearch.cb.getDocTfidfVector(73)
-vec2_tfidf = ssearch.cb.getDocTfidfVector(results[0][0])
-
-# Interpret the top match.
-ssearch.interpretMatch(vec1_tfidf, vec2_tfidf)
-
+print('\n======== Example 1 ========\n')
+runExample1(ssearch)
+print('\n======== Example 2 ========\n')
+runExample2(ssearch)
